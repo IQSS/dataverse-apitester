4f05eae6a60a975c0cfbfcf2ce48c51bf8136703
Philip Durbin <philip_durbin@harvard.edu>
2015-04-06 15:14:13 -0400

    more refactoring, stub out delete files test

--- readme.md
+++ readme.md
@@ -3,3 +3,7 @@
 https://build.hmdc.harvard.edu:8443/job/dataverse-apitester/
 
 mvn clean test -Dapitester.baseuri="http://apitest.dataverse.org" -Dtest=MainTest,SwordTest
+
+mvn clean test -Dtest=MainTest,SwordTest
+
+curl -X DELETE http://localhost:8080/api/admin/authenticatedUsers/saber
--- src/test/java/org/dataverse/apitester/sword/SwordTest.java
+++ src/test/java/org/dataverse/apitester/sword/SwordTest.java
@@ -29,9 +29,11 @@ public class SwordTest {
     static String EMPTY_STRING = "";
     private static String globalId1;
     private static String globalId2;
+    private static String globalId3;
     final String expectedSwordSpecVersion = "2.0";
     static String dvAlias1 = "swordtestdv1";
     static String dvAlias2 = "swordtestdv2";
+    static String dvAlias3 = "swordtestdv3";
     static String datasetSwordIdUrl;
 
     @BeforeClass
@@ -130,6 +132,35 @@ public class SwordTest {
 
     }
 
+    @Test
+    public void testSwordDeleteFiles() throws IOException {
+        // create dataverse
+        JsonArrayBuilder contactArrayBuilder = Json.createArrayBuilder();
+        contactArrayBuilder.add(Json.createObjectBuilder().add("contactEmail", "tom@mailinator.com"));
+        JsonArrayBuilder subjectArrayBuilder = Json.createArrayBuilder();
+        subjectArrayBuilder.add("Other");
+        JsonObject dvData = Json.createObjectBuilder().add("alias", dvAlias3).add("name", dvAlias3).add("dataverseContacts", contactArrayBuilder).add("dataverseSubjects", subjectArrayBuilder).build();
+
+        Response createDataverseResponse = given().body(dvData.toString()).contentType(ContentType.JSON).when().post("/api/dataverses/:root?key=" + apiToken);
+        assertEquals(201, createDataverseResponse.getStatusCode());
+        JsonPath jsonPath = JsonPath.from(createDataverseResponse.body().asString());
+        File datasetXml = new File("src/test/java/org/dataverse/apitester/sword/data/dataset-trees1.xml");
+        String xmlIn = Files.toString(datasetXml, StandardCharsets.UTF_8);
+
+        Response createDatasetResponse = given()
+                .auth().basic(apiToken, EMPTY_STRING)
+                .body(xmlIn)
+                .contentType("application/atom+xml")
+                .post("/dvn/api/data-deposit/v1.1/swordv2/collection/dataverse/" + dvAlias3);
+        String xml = createDatasetResponse.body().asString();
+        assertEquals(201, createDatasetResponse.getStatusCode());
+        datasetSwordIdUrl = from(xml).get("entry.id");
+        /**
+         * @todo stop assuming the last 22 characters are the doi/globalId
+         */
+        globalId3 = datasetSwordIdUrl.substring(datasetSwordIdUrl.length() - 22);
+    }
+
     @AfterClass
     public static void cleanUp() {
 
@@ -147,6 +178,11 @@ public class SwordTest {
         assertEquals(204, deleteDataset2Response.getStatusCode());
         Response deleteDataverse2Response = deleteDataverse(dvAlias2);
         assertEquals(200, deleteDataverse2Response.getStatusCode());
+
+        Response deleteDataset3Response = deleteDataset(globalId3);
+        assertEquals(204, deleteDataset3Response.getStatusCode());
+        Response deleteDataverse3Response = deleteDataverse(dvAlias3);
+        assertEquals(200, deleteDataverse3Response.getStatusCode());
 
         Response deleteUserResponse = given().delete("/api/admin/authenticatedUsers/" + username + "/");
         assertEquals(200, deleteUserResponse.getStatusCode());
